3b8c8f50253fb1b40018b1e53252fd376a9516f7
kennethbchen <kennethbchen@gmail.com>
2022-07-16 18:24:44 -0400

    Scrapped non essential things

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Resources Used
 SoloByte's Godot Fracturing Library - https://github.com/SoloByte/godot-polygon2d-fracture
 
 Object Dragging Implementation Methodology from KidsCanCode - https://kidscancode.org/godot_recipes/physics/rigidbody_drag_drop/
+
+Kenney Future Font - https://www.kenney.nl/assets/kenney-fonts
--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -6,10 +6,20 @@ onready var label = $Label
 onready var timer = $Timer
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 func _process(delta):
-	label.text = str(int(timer.time_left))
+
+	
+	if timer.time_left > 1:
+		label.align = HALIGN_CENTER
+		label.text = ("%d" % timer.time_left)
+	else:
+		label.align = HALIGN_LEFT
+		label.text = ("%0.2f" % timer.time_left)
+
+	
